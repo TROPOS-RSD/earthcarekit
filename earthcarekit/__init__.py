310f39f298649765b64214b14b5b5ce5253451a4
Leonard König <koenig@tropos.de>
2025-07-30 10:44:43 +0200

    Set new version tag to 0.1.1rc1

--- earthcarekit/__init__.py
+++ earthcarekit/__init__.py
@@ -11,7 +11,7 @@ Licensed under the MIT License (see LICENSE file or https://opensource.org/licen
 
 __author__ = "Leonard König"
 __license__ = "MIT"
-__version__ = "0.1.0"
+__version__ = "0.1.1rc1"
 __date__ = "2025-07-28"
 __maintainer__ = "Leonard König"
 __email__ = "koenig@tropos.de"
