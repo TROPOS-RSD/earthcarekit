ffa269c86b2c6e35a30506a86f3f4675e6df2120
Leonard König <koenig@tropos.de>
2025-08-08 15:29:30 +0200

    Bump version to 0.1.2rc1

--- earthcarekit/__init__.py
+++ earthcarekit/__init__.py
@@ -11,7 +11,7 @@ Licensed under the MIT License (see LICENSE file or https://opensource.org/licen
 
 __author__ = "Leonard König"
 __license__ = "MIT"
-__version__ = "0.1.1"
+__version__ = "0.1.2rc1"
 __date__ = "2025-08-08"
 __maintainer__ = "Leonard König"
 __email__ = "koenig@tropos.de"
