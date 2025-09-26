07c3036eaa6417ab997fb0dafcf1cb2feb8bfca6
Leonard König <koenig@tropos.de>
2025-09-26 12:08:32 +0200

    Bump version to 0.7.1

--- earthcarekit/__init__.py
+++ earthcarekit/__init__.py
@@ -19,8 +19,8 @@ Copyright (c) 2025 Leonard König
 
 __author__ = "Leonard König"
 __license__ = "MIT"
-__version__ = "0.7.0"
-__date__ = "2025-09-25"
+__version__ = "0.7.1"
+__date__ = "2025-09-26"
 __maintainer__ = "Leonard König"
 __email__ = "koenig@tropos.de"
 __title__ = "earthcarekit"
