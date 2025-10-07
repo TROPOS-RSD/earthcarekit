67184cf6132b5204f823dfafb81088794b95b340
Leonard König <koenig@tropos.de>
2025-10-07 15:16:35 +0200

    Update default colorbar width

--- earthcarekit/utils/constants/plotting.py
+++ earthcarekit/utils/constants/plotting.py
@@ -3,4 +3,4 @@ from typing import Final
 DEFAULT_CMAP: Final[str] = "viridis"
 DEFAULT_PROFILE_SHOW_STEPS: bool = True
 
-DEFAULT_COLORBAR_WIDTH: Final[float] = 0.16
+DEFAULT_COLORBAR_WIDTH: Final[float] = 0.14
