21cd2980067b8e8f5f1b4fa4c53406e7ea82f9f0
Leonard König <koenig@tropos.de>
2025-12-18 15:27:15 +0100

    Added xarray utility function for adding new variables to a dataset at a specific index or before/after another variable

--- earthcarekit/utils/xarray_utils/__init__.py
+++ earthcarekit/utils/xarray_utils/__init__.py
@@ -4,5 +4,6 @@ from .exception import EmptyFilterResultError
 from .filter_latitude import filter_latitude
 from .filter_radius import filter_radius
 from .filter_time import filter_time, get_filter_time_mask
+from .insert_var import insert_var
 from .merge import merge_datasets
 from .scalars import convert_scalar_var_to_str
