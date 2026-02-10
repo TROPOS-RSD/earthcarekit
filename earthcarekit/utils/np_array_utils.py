cb32798d2292127bed3714ef946d02eadb6a8df0
Leonard König <koenig@tropos.de>
2026-02-10 11:36:36 +0100

    Added new array utils funtion pad_true_sequence_2d

--- earthcarekit/utils/np_array_utils.py
+++ earthcarekit/utils/np_array_utils.py
@@ -318,6 +318,42 @@ def pad_true_sequence(
     return mask
 
 
+def pad_true_sequence_2d(
+    a: NDArray[np.bool_],
+    n: int,
+    axis: int = 1,
+) -> NDArray[np.bool_]:
+    """Pads all sequences of True values occuring along one axis in an 2d array with n True values before and after the sequence (while keeping the original size)."""
+
+    if axis not in [0, 1]:
+        raise ValueError(f"invalid axis ({axis}), expected 0 or 1")
+
+    if axis == 0:
+        a = a.T
+
+    ncols = a.shape[1]
+    idx = np.arange(ncols)
+
+    any_true = np.asarray(a.any(axis=1))
+
+    first_true = np.argmax(a, axis=1)
+    first_true = np.where(any_true, first_true, ncols)
+
+    last_true = ncols - 1 - np.argmax(a[:, ::-1], axis=1)
+    last_true = np.where(any_true, last_true, -1)
+
+    start = np.clip(first_true - n, 0, ncols)
+    end = np.clip(last_true + n + 1, 0, ncols)
+
+    mask = (idx >= start[:, np.newaxis]) & (idx < end[:, np.newaxis])
+    mask &= any_true[:, np.newaxis]
+
+    if axis == 0:
+        mask = mask.T
+
+    return mask
+
+
 def get_most_freq_int(a: ArrayLike):
     a = np.asarray(a)
     min_val = a.min()
