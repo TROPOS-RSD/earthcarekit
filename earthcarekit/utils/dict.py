e9f6888f21cba7417bc4bc0eb3f6ec7ba0ba260d
Leonard König <koenig@tropos.de>
2026-04-22 11:31:47 +0200

    Added dict inversion utils

--- earthcarekit/utils/dict.py
+++ earthcarekit/utils/dict.py
@@ -1,7 +1,26 @@
-def remove_keys_from_dict(d: dict, keys: list) -> dict:
+from typing import TypeVar
+
+A = TypeVar("A")
+B = TypeVar("B")
+
+
+def remove_keys_from_dict(d: dict[A, B], keys: list[A]) -> dict[A, B]:
     """Return new dictionary with selected keys removed."""
     d = d.copy()
     for k in keys:
         if k in d:
             del d[k]
     return d
+
+
+def invert_dict(d: dict[A, B]) -> dict[B, A]:
+    """Return new dictionary with keys and values swapped (assumes all unique values)."""
+    return {v: k for k, v in d.items()}
+
+
+def invert_dict_nonunique(d: dict[A, B]) -> dict[B, list[A]]:
+    """Return new dictionary mapping from values to lists of keys that map to it."""
+    inv_d: dict[B, list[A]] = {}
+    for k, v in d.items():
+        inv_d.setdefault(v, []).append(k)
+    return inv_d
